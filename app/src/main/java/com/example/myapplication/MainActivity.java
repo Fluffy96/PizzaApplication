5b2e7134f5318865b35c839fedbb3d91884a0608
Fluffy96 <52479039+Fluffy96@users.noreply.github.com>
2021-12-04 19:05:37 -0500

    12/4 7:05 pm Viraj

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -25,31 +27,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Intent intent = new Intent(this, PizzaOrderActivity.class);
         String pizzaType = "deluxe";
         intent.putExtra("PIZZA", pizzaType);
-        this.startActivity(intent);
+        intent.putExtra("NUMBER", phoneNumber.toString());
+        if(numberChecker(phoneNumber))
+            this.startActivity(intent);
     }
 
     public void onHawaiianClick(View view){
         Intent intent = new Intent(this, PizzaOrderActivity.class);
         String pizzaType = "hawaiian";
         intent.putExtra("PIZZA", pizzaType);
-        this.startActivity(intent);
+        if(numberChecker(phoneNumber))
+            this.startActivity(intent);
     }
 
     public void onPepperoniClick(View view){
         Intent intent = new Intent(this, PizzaOrderActivity.class);
         String pizzaType = "pepperoni";
         intent.putExtra("PIZZA", pizzaType);
-        this.startActivity(intent);
+        if(numberChecker(phoneNumber))
+            this.startActivity(intent);
     }
 
     public void onCOrderClick(View view){
         Intent intent = new Intent(this, CurrentOrderActivity.class);
-        this.startActivity(intent);
+        if(numberChecker(phoneNumber))
+            this.startActivity(intent);
     }
 
     public void onSOrderClick(View view){
         Intent intent = new Intent(this, StoreOrdersActivity.class);
-        this.startActivity(intent);
+        if(numberChecker(phoneNumber))
+            this.startActivity(intent);
+    }
+
+    public boolean numberChecker(TextView t){
+        String num = t.toString();
+        if(num.length() == 10){
+            return true;
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Invalid Phone Number",Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 
     @Override
